dfb36d15c77fd9e36476dfbf9bb87cbb7d2b34ca
omar <omarasaaam@gmail.com>
2023-11-25 23:26:56 +0200

    creating the data structure for the book card

---
+++ Back-end/script.js
@@ -0,0 +1,14 @@
+let booksCard=[{
+    image:"link",
+    categori:"sting",
+    BookName:"string" ,
+    rating:
+    {
+        stars:"int",
+        count:"int"
+    },
+    price:"int"
+}]
+array.forEach(book => {
+    
+});
