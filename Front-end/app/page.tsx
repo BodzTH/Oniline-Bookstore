831654fc210e1a8cc7b08d6b0f5365f21c8d79a1
Abdelrahman Tharwat <abdelrahman_thr2002@hotmail.com>
2023-11-26 09:42:55 +0200

    jsx

--- Front-end/app/page.tsx
+++ Front-end/app/page.tsx
@@ -1,21 +1,31 @@
+"use client";
 import Image from 'next/image'
 import styles from './page.module.css'
-
-function Header() {
-return (
-<header className={styles.main}>
-  <h2>Bye World</h2>
-</header>
-);
+import Link from 'next/link';
+import { useState } from 'react';
+import Header from './components/header';
 
 
-}
 
 function Home() {
+
+
+  const [counter, setCounter]=useState(0);
+
+  const handleClick = () => {
+    setCounter(counter +1);
+  }
+
+
   return (
-    <main className={styles.main}>
+    <main>
      <h1>Hello World</h1>
      <Header/>
+     <Link href="/about">Go To About</Link>
+     <br />
+     <a href="/about">duud</a>
+     <div> Counter : {counter}</div>
+     <button onClick={handleClick}>incerment</button>
     </main>
   );
 }
