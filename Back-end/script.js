d771cf5940a04ca04bb2d88cba532e1783b71d72
omar <omarasaaam@gmail.com>
2023-11-28 15:37:57 +0200

    adding interactive add to cart adding cart button with the quantity in the cart

--- Back-end/script.js
+++ Back-end/script.js
@@ -1,4 +1,5 @@
 let bookscard=[{
+    id:1,
     categori:"action",
     BookName:"my journy" ,
     rating:
@@ -10,8 +11,9 @@ let bookscard=[{
     instock:10,
     sold:2,
 },{
+    id:2,
     categori:"action",
-    BookName:"my journy" ,
+    BookName:"journey to the center of the earth" ,
     rating:
     {
         stars:4.5,
@@ -19,18 +21,51 @@ let bookscard=[{
     },
     priceCents:1000
 }]
-console.log("omar")
  let productsHTML=``
 bookscard.forEach(book => {
     productsHTML+=`
     <div>${book.BookName}</div>
     <div>${book.rating.count}</div>
     <div>${book.priceCents/100}</div>
-    <button class="js-add-to-cart">add to cart</button>
+    <button class="js-add-to-cart" data-book-id="${book.id}" >add to cart</button>
     
 `
 });
 document.querySelector(".js-products-grid").innerHTML=productsHTML;
-console.log("omar")
-console.log(productsHTML);
-document.querySelector(".js-add-to-cart")
+document.querySelectorAll(".js-add-to-cart").forEach(button => {
+    button.addEventListener('click',() =>{
+        const bookid =button.dataset.bookId
+        let matchingBook;
+        cart.forEach(book => {
+            if(bookid==book.ID)
+            {
+                
+                matchingBook=book
+            }
+
+        });
+        if(matchingBook)
+        {
+            matchingBook.quantity+=1
+        }
+        else
+        {
+        cart.push({
+            ID:bookid,
+            quantity:1
+        })
+        }
+        let cartQuantity=0;
+        cart.forEach(book => {
+            cartQuantity += book.quantity
+        });
+        document.querySelector(".js-cart-qantity").innerHTML=cartQuantity
+        console.log(cartQuantity)
+
+        console.log(cart)
+        
+    })
+
+
+    
+});
