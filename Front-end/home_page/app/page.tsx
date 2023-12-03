dbc1fdba511e3e7f1f29edce5a5c829524a97d51
Abdelrahman Tharwat <abdelrahman_thr2002@hotmail.com>
2023-12-03 20:28:40 +0200

    A new f start

---
+++ Front-end/home_page/app/page.tsx
@@ -0,0 +1,17 @@
+import CategoriesRow from "@/components/CategoriesRow";
+
+function Home() {
+  return (
+    <main>
+
+<section>
+
+<CategoriesRow title={"English Books"} books={["lol"]} name={"DEad"} image={""} price={"500"} cart={"ss"}/>
+
+</section>  
+      
+    </main>
+  )
+}
+
+export default Home;
